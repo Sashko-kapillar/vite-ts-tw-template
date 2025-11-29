74b74953e0296f12a91e2b6f2d5f3c652f166b5c
kapillar <kapillarua@gmail.com>
2025-11-29 16:35:05 +0200

    feat: create a template for React + TS

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,34 @@
+import { defineConfig } from 'eslint'
 import js from '@eslint/js'
 import globals from 'globals'
+import tseslint from 'typescript-eslint'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  {
+    ignores: ['dist', 'node_modules'],
+  },
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    extends: [
+      js.configs.recommended, // базові правила JS
+      tseslint.configs.recommended, // оптимальні правила TS (не strict)
+      reactHooks.configs.flat.recommended, // правила React hooks
+      reactRefresh.configs.vite, // підтримка React Refresh
+      'plugin:prettier/recommended', // інтеграція Prettier + ESLint
+    ],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': ['error'],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
 ])
